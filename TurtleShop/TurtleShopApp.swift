109ce67ed4fc3e00076f7a6f33a3651c0d8a4e46
Chris Turner <chris_turner@icloud.com>
2024-06-25 18:00:24 +0100

    Initial Commit

---
+++ TurtleShop/TurtleShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurtleShopApp.swift
+//  TurtleShop
+//
+//  Created by Chris Turner on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TurtleShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
