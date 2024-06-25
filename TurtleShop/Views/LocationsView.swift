2d4df900cc5ffcc047e9785bf174ae2aae93eb75
Chris Turner <chris_turner@icloud.com>
2024-06-25 19:44:29 +0100

    Added tab bar at the bottom

---
+++ TurtleShop/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  TurtleShop
+//
+//  Created by Chris Turner on 25/06/2024.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
